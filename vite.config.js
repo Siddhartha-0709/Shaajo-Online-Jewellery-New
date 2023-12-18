5d62c13d2f002977625abe31861c5c55cde08845
Siddhartha Mukherjee <104203643+Siddhartha-0709@users.noreply.github.com>
2023-12-18 13:47:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'http://localhost:3000'
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000',
     },
-    server: {
     host: '0.0.0.0',
     port: 3000, // Adjust the port as needed
-  }
   },
   plugins: [react()],
-})
+});
